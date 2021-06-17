77604e735b4350887de93211f44a17ace5c1db70
Dien Nguyen <khanhdien@gmail.com>
2021-06-17 10:29:30 +0700

    rename path

--- packages/uikit/src/components/PancakeToggle/index.stories.tsx
+++ packages/uikit/src/components/PancakeToggle/index.stories.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
 import PancakeToggle from "./PancakeToggle";
 
-export default {
-  title: "Components/PancakeToggle",
-  component: PancakeToggle,
-};
+// export default {
+//   title: "Components/PancakeToggle",
+//   component: PancakeToggle,
+// };
 
 export const Default: React.FC = () => {
   const [isChecked, setIsChecked] = useState(false);
